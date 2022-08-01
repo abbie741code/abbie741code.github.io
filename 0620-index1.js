3deef4c642d2be2463230ac143384cfa16483974
Abbie <67620726+abbie741code@users.noreply.github.com>
2022-08-01 10:22:50 +0800

    initial commit
    test

---
+++ 0620-index1.js
@@ -0,0 +1,58 @@
+console.log(100)
+let myName1 = "annie"
+console.log(myName1)
+
+
+
+
+let title = '提示訊息:'      
+let title1 = '錯誤'
+let title2 = '成功'
+
+let alert1 = title + title1
+let alert2 = title + title2
+            // (不是加法;是連接的意思)
+console.log(alert1)
+console.log(alert2)
+
+
+
+
+let myName = 'abbie'
+let age = '我的年齡：999歲'
+
+let word = myName + age
+// (不是加法;是連接的意思)
+console.log(word)
+
+
+//learning on 6/26
+let score = 100
+console.log(score - 100)
+console.log(score % 6) 
+// 🌟取餘數
+
+
+// let(新語法, 基本上可以取代var)
+// var
+// const（不可重複宣告？）
+
+
+// const可增加程式碼的穩定性,不可亂改變數(出錯)
+const a2 = "ccc"
+console.log(a2)
+// 修改a1的內容
+a1 = "ddd"
+console.log(a2)
+// -----------------
+
+let a1 = "aaa"
+console.log(a1)
+// 修改a1的內容
+a1 = "bbb"
+console.log(a1)
+
+// -----------------
+
+
+
---
+++ 0626-index2.js
@@ -0,0 +1,19 @@
+// 陣列array
+// learning on 6/26
+
+
+
+
+let classA = ['小白', '小明', '小華']
+console.log('轉學前：', classA)
+
+classA.push('米寶')
+// 往這個陣列後面再加一個學生
+console.log('轉學後：', classA)
+
+
+const arr1 = [1, 2, 3]
+console.log('arr1')
+
+
+
